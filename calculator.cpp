50d6f4f993b9f3f471d32828adac457d551f5a70
hüsne ebrar erturhan <erturhanebrar05@gmail.com>
2025-10-30 15:32:45 +0300

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -1,20 +1,38 @@
 #include <iostream>
-
 using namespace std;
-int main(){
-  
-    int sayi1, sayi2, toplam;
 
-    cout<<"Birinci sayiyi giriniz: " << endl;
-    cin>> sayi1;
+// Toplama
+int toplama(int a, int b) {
+    return a + b;
+}
 
+// Çıkarma
+int cikarma(int a, int b) {
+    return a - b;
+}
 
-    cout<<"Ikinci sayiyi giriniz" << endl;
-    cin>> sayi2;
+// Çarpma
+int carpma(int a, int b) {
+    return a * b;
+}
 
-    toplam = sayi1 + sayi2;
-    cout<< "Toplam degeri: "<< toplam << endl;
+// Bölme
+double bolme(double a, double b) {
+    if(b != 0)
+        return a / b;
+    else {
+        cout << "Sıfıra bölünemez!" << endl;
+        return 0;
+    }
+}
 
+int main() {
+    int a = 10, b = 5;
+
+    cout << "Toplama: " << toplama(a, b) << endl;
+    cout << "Çıkarma: " << cikarma(a, b) << endl;
+    cout << "Çarpma: " << carpma(a, b) << endl;
+    cout << "Bölme: " << bolme(a, b) << endl;
 
     return 0;
 }
