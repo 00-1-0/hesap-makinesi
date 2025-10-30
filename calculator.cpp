49e5c3bcf58262108eeb6b5e3a7e79e8d0fd130f
hüsne ebrar erturhan <erturhanebrar05@gmail.com>
2025-10-30 15:26:17 +0300

    Add basic calculator for summing two integers

---
+++ calculator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+  
+    int sayi1, sayi2, toplam;
+
+    cout<<"Birinci sayiyi giriniz: " << endl;
+    cin>> sayi1;
+
+
+    cout<<"Ikinci sayiyi giriniz" << endl;
+    cin>> sayi2;
+
+    toplam = sayi1 + sayi2;
+    cout<< "Toplam degeri: "<< toplam << endl;
+
+
+    return 0;
+}
